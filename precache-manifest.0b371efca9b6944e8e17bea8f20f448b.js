88c39087fddbeeacebc152f7f73af249c1b73ed4
muhammad rizwan raza <m.rizwan0549@gmail.com>
2024-10-20 02:21:00 +0500

    first commit

---
+++ precache-manifest.0b371efca9b6944e8e17bea8f20f448b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa621108a58456d851a65ccfb4c9e5ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38170291216c6d3459cb",
+    "url": "/static/css/main.2eee9858.chunk.css"
+  },
+  {
+    "revision": "a2da0c83dd28211e81fa",
+    "url": "/static/js/2.3d712c94.chunk.js"
+  },
+  {
+    "revision": "8c3214d3d6e31cb577d7646cc2e8506b",
+    "url": "/static/js/2.3d712c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442f0483332e8633290c",
+    "url": "/static/js/3.b9ea6e8f.chunk.js"
+  },
+  {
+    "revision": "38170291216c6d3459cb",
+    "url": "/static/js/main.7244cc2b.chunk.js"
+  },
+  {
+    "revision": "8ee4acbb512d78f6e68c",
+    "url": "/static/js/runtime-main.0ccd9583.js"
+  },
+  {
+    "revision": "1e9a0562c4ff2ef7a1da81b3a4a0f5b6",
+    "url": "/static/media/chapter.1e9a0562.svg"
+  },
+  {
+    "revision": "d01924b194ce93cdb175c269e63e444d",
+    "url": "/static/media/dummy-thumbnail.d01924b1.jpg"
+  },
+  {
+    "revision": "10b830bff2059285c8e5e8bf285a0689",
+    "url": "/static/media/home.10b830bf.svg"
+  },
+  {
+    "revision": "98285481c550fbe04d3cc61a2a79d837",
+    "url": "/static/media/language.98285481.svg"
+  },
+  {
+    "revision": "2f4f499ecefb2208c23940ae58ea6078",
+    "url": "/static/media/logo-horizontal.2f4f499e.png"
+  },
+  {
+    "revision": "ea83b7d166478c645258978e566b9eca",
+    "url": "/static/media/logo.ea83b7d1.png"
+  },
+  {
+    "revision": "0add3a6928acf10c4327d033ff1edcdc",
+    "url": "/static/media/logout.0add3a69.svg"
+  },
+  {
+    "revision": "d9e35bfa52350e6cdcc1d8c3c6f7851e",
+    "url": "/static/media/online.d9e35bfa.svg"
+  },
+  {
+    "revision": "1b253a3624193dd72da5961e42f796cb",
+    "url": "/static/media/progress.1b253a36.svg"
+  },
+  {
+    "revision": "85424d355a2596d83c221aab4a10a9aa",
+    "url": "/static/media/quiz.85424d35.svg"
+  },
+  {
+    "revision": "db27acb81ca7a1510f86ac7076aa046b",
+    "url": "/static/media/schedule.db27acb8.svg"
+  }
+]);
